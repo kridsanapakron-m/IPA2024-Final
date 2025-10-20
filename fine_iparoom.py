cf218e57866a12efc5d577e89a621fcb3a4b210e
kridsanapakron <dgsx.64854@gmail.com>
2025-10-20 15:11:28 +0700

    create find room

---
+++ fine_iparoom.py
@@ -0,0 +1,18 @@
+import requests
+import os
+load_dotenv()
+
+def get_room_id(title):
+    ACCESS_TOKEN = os.getenv("WEBEX_ACCESS_TOKEN")
+    url = "https://webexapis.com/v1/rooms"
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        rooms = response.json()["items"]
+        for room in rooms:
+            if room["title"] == title:
+                return room["id"]
+    return None
+
+
